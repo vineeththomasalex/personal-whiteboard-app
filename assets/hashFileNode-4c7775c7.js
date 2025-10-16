34212b9a73fbcc518c8e5776ca5ae6c73732f8e5
Vineeth Thomas Alex <vithoma@microsoft.com>
2025-10-16 13:22:29 -0500

    broadcast update

---
+++ assets/hashFileNode-4c7775c7.js
@@ -0,0 +1,4 @@
+import{s as a,a as i}from"./sha256-2b2d765b.js";import"./index-76efaf4f.js";/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */async function c(e,s="SHA-1",t="hex"){let n;switch(s){case"SHA-1":{n=new i;break}case"SHA-256":{n=new a;break}}return n.update(e).digest(t)}async function h(e){const t=`blob ${e.byteLength.toString()}${String.fromCharCode(0)}`;return new i().update(t).update(e).digest("hex")}export{h as gitHashFile,c as hashFile};
